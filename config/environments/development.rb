d02e191219983cb01ad807c06dfdaa091cec2f41
Mark Modak <mmodak@mmodak.local>
2019-05-05 19:36:13 -0600

    2nd try for production email.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => Rails.application.credentials[:sendgrid][:user_name],
+    :password => Rails.application.credentials[:sendgrid][:password],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
